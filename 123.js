a2c38a37003afdac9cb330911a06e7a3ca3eb361
Lưu Văn Đức <50072440+luuvanduc1999@users.noreply.github.com>
2020-01-21 17:50:22 +0700

    Update 123.js

--- 123.js
+++ 123.js
@@ -1,13 +1,16 @@
 document.onkeyup = function(e) {
   if (e.which == 49) {
+    //KHÔNG
     C=document.getElementsByClassName("_5r7k _5r7m _2p24");
     for (var i=0; i<C.length; i++)
         if (C[i].children[0].textContent=="Không") C[i].children[1].click();
   } else if (e.which == 50) {
+    //KHÔNG CHẮC CHẮN
     C=document.getElementsByClassName("_5r7k _5r7m _2p24");
     for (var i=0; i<C.length; i++)
         if (C[i].children[0].textContent=="Không chắc chắn") C[i].children[1].click();
   } else if (e.which == 51) {
+    //CÓ
     C=document.getElementsByClassName("_5r7k _5r7m _2p24");
     for (var i=0; i<C.length; i++)
         if (C[i].children[0].textContent=="Có") C[i].children[1].click();
